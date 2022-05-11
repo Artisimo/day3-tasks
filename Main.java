e48727f35b5c6fa8eb1ada403a1e53b1df98da47
Artis Birznieks <birznieks.artis@gmail.com>
2022-05-11 10:41:23 +0300

    Added another array and a way to copy it

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.concurrent.ThreadLocalRandom;
 public class Main {
     public static void main(String[] args) {
         int[][] multiplicationTable = new int[10][10];    // array and stores multiplication table (from 1 - 10) in it.
@@ -6,6 +7,19 @@ public class Main {
                 multiplicationTable[i][j] = (i+1)*(j+1);
                 System.out.println((i +1) + " * " + (j + 1) + " = " + multiplicationTable[i][j]);
             }
+        }
+
+        int[] toCopy = new int[4];
+        System.out.println("First array: ");
+        for(int i = 0; i < 4; i++){                   // Initialize array with four arbitrary integers.
+            toCopy[i] = ThreadLocalRandom.current().nextInt(1, 100 + 1);
+            System.out.println(toCopy[i]);
+        }
+
+        System.out.println("Copied array: ");
+        int[] copiedArray = toCopy;    // Copy the array.
+        for(int i = 0; i < 4; i++){
+            System.out.println(copiedArray[i]);
         }
     }
 }
