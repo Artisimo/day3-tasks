10d7d1b72c8d048b295fef132197cd76b0d0f15b
Artis Birznieks <birznieks.artis@gmail.com>
2022-05-11 10:32:52 +0300

    Created a program which stores the multiplication table in a 2d array

--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        
-
+        int[][] multiplicationTable = new int[10][10];    // array and stores multiplication table (from 1 - 10) in it.
+        for (int i = 0; i < 10; i++){
+            for(int j = 0; j < 10; j++){
+                multiplicationTable[i][j] = (i+1)*(j+1);
+                System.out.println((i +1) + " * " + (j + 1) + " = " + multiplicationTable[i][j]);
+            }
+        }
     }
 }
