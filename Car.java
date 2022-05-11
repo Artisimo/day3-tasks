15c09ec643dd657d047147a6cf43125ffba396a5
Artis Birznieks <birznieks.artis@gmail.com>
2022-05-11 12:38:35 +0300

    Added a car classs which inherits from Vehicle class

--- Car.java
+++ Car.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Car {
+public class Car extends Vehicle{
+    protected String model;
+
+    public Car(String brand, String color, int topSpeed, int nrOfGears, String model){
+        super(brand, color, topSpeed, nrOfGears);
+        this.model = model;
+    }
+    public void printProperties(){
+        super.printProperties();
+        System.out.println("Model: " + this.model + '\n');
+    }
+
+    public static void main(String[] args) {
+        Car car = new Car("BMW", "Red", 200, 6,"335i");
+        car.printProperties();
+        car.setGear(-1);
+        car.setSpeed(10);
+        car.stop();
+        car.setGear(1);
+        car.setSpeed(50);
+        car.setGear(2);
+        car.turnLeft();
+        car.stop();
+        car.printProperties();
+    }
 }
